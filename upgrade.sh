ca2e5921c8b65b6c466ffc191412b4ea17249e8e
Michael <30936296+mtan93@users.noreply.github.com>
2020-07-16 16:26:22 +0100

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -8,7 +8,7 @@ echo Deactivating Old Workers
 echo
 wp plugin deactivate worker --path=$HMEPATH/public_html/ && wp plugin delete worker --path=$HMEPATH/public_html/
 wp plugin deactivate auocloudworker --path=$HMEPATH/public_html/ && wp plugin delete auocloudworker --path=$HMEPATH/public_html/
-sleeps 1s
+sleep 1s
 echo Installing New Workers
 wp plugin install https://downloads.wordpress.org/plugin/mainwp-child.4.0.7.1.zip --path=$HMEPATH/public_html/
 wp plugin activate mainwp-child --path=$HMEPATH/public_html/
